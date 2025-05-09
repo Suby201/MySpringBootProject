5deab605299bc70e8f25b40896f56a289eebcf18
Suby201 <flyingcar37@naver.com>
2025-05-09 13:28:23 +0900

    5.9 class

--- src/main/java/com/basic/myspringboot/entity/User.java
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -1,6 +1,9 @@
 package com.basic.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,8 +19,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
+    @NotBlank(message = "Name is Essential")
     private String name;
     @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is mandatory")
+    @Email
     private String email;
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
